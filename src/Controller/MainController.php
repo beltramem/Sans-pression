4fd59a6618a4eb0f01e4b7dab373a3d582beaff4
unknown <beltramo.emeric@gmail.com>
2018-04-10 15:31:22 +0200

    essai produit home page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+
 class MainController extends Controller
 {
     /**
