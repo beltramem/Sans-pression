09c64ee782e3eac346d7b7a1aee0ab7cb0aaaf03
unknown <beltramo.emeric@gmail.com>
2018-04-11 10:45:36 +0200

    menu homme fonctionnel

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,6 @@
 function menuLeft(){
 	if ($('.home-type-produit').text()=="Nos bières")
 	{
-		console.log('bite')
 		$('.home-type-produit').text("Divers");
 		$(".section-produit-biere").toggle();
 		$(".section-produit-diver").toggle();
@@ -37,9 +36,51 @@ function menuLeft(){
 		$(".section-produit-biere").toggle();
 	}
 }
+function menuRight(){
+	if ($('.home-type-produit').text()=="Nos bières")
+	{
+		$('.home-type-produit').text("à la tireuse");
+		$(".section-produit-biere").toggle();
+		$(".section-produit-tireuse").toggle();
+	}	
+	else if ($('.home-type-produit').text()=="à la tireuse")
+	{
+		$('.home-type-produit').text("Nos vins");
+		$(".section-produit-tireuse").toggle();
+		$(".section-produit-vin").toggle();
+	}	
+	else if ($('.home-type-produit').text()=="Nos vins")
+	{
+		$('.home-type-produit').text("Nos alcools");
+		$(".section-produit-vin").toggle();
+		$(".section-produit-alcool").toggle();
+	}	
+	else if ($('.home-type-produit').text()=="Nos alcools")
+	{
+		$('.home-type-produit').text("Nos verres");
+		$(".section-produit-alcool").toggle();
+		$(".section-produit-verre").toggle();
+	}	
+	else if ($('.home-type-produit').text()=="Nos verres")
+	{
+		$('.home-type-produit').text("Divers");
+		$(".section-produit-verre").toggle();
+		$(".section-produit-diver").toggle();
+	}	
+	else if ($('.home-type-produit').text()=="Divers")
+	{
+		$('.home-type-produit').text("Nos bières");
+		$(".section-produit-diver").toggle();
+		$(".section-produit-biere").toggle();
+	}
+}
 
 $(document).ready(function() {
 	$(".glyphicon-menu-left").click(function() {
 		menuLeft();
-	});}
+	});
+	$(".glyphicon-menu-right").click(function() {
+		menuRight();
+	});
+	}
 );
